b353d1aaa08c0a16753a935e5e7b67cefdd405ae
Josh Sommer <JoshDSommer@gmail.com>
2017-06-22 19:59:55 -0400

    creating new 3.0 demo project

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,3 +1,15 @@
-﻿import * as application from 'application';
+﻿/*
+In NativeScript, the app.ts file is the entry point to your application.
+You can use this file to perform app-level initialization, but the primary
+purpose of the file is to pass control to the app’s first module.
+*/
 
-application.start({ moduleName: 'loader' });
+import "./bundle-config";
+import * as app from 'application';
+
+app.start({ moduleName: 'main-page' });
+
+/*
+Do not place any code after the application has been started as it will not
+be executed on iOS.
+*/
